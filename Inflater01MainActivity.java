0e3613f2a16771a00095ba7812ffa700b770eb35
sinaburokim8725 <sinaburokim8725@gmail.com>
2018-10-09 02:31:41 +0900

    처음 올려봅니다.

--- Inflater01MainActivity.java
+++ Inflater01MainActivity.java
@@ -9,5 +9,6 @@ public class Inflater01MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inflater01_main);
+        //
     }
 }
